44aa78fc78a616b5df7c31b7d2ece53f6b5f180b
Ethan <weiqiang1994.cwq@gmail.com>
2019-03-15 22:35:03 +0800

    Done with rank page list view

--- app/src/main/java/com/ardiya/simpleweather/RankFragment.java
+++ app/src/main/java/com/ardiya/simpleweather/RankFragment.java
@@ -6,6 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -22,8 +28,42 @@ public class RankFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rank, container, false);
+        String[] listviewTitle = new String[]{
+                "ListView Title 1", "ListView Title 2", "ListView Title 3", "ListView Title 4",
+                "ListView Title 5", "ListView Title 6", "ListView Title 7", "ListView Title 8",
+        };
+
+
+        int[] listviewImage = new int[]{
+                R.drawable.rankusers, R.drawable.rankusers, R.drawable.rankusers, R.drawable.rankusers,
+                R.drawable.rankusers, R.drawable.rankusers, R.drawable.rankusers, R.drawable.rankusers,
+        };
+
+        String[] listviewShortDescription = new String[]{
+                "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+                "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+        };
+
+        View view = inflater.inflate(R.layout.fragment_addfriend, container, false);
+
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+
+        for (int i = 0; i < 8; i++) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("listview_title", listviewTitle[i]);
+            hm.put("listview_discription", listviewShortDescription[i]);
+            hm.put("listview_image", Integer.toString(listviewImage[i]));
+            aList.add(hm);
+        }
+
+        String[] from = {"listview_image", "listview_title", "listview_discription"};
+        int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(), aList, R.layout.listview_activity, from, to);
+        ListView androidListView = (ListView) view.findViewById(R.id.listview);
+        androidListView.setAdapter(simpleAdapter);
+
+        return view;
     }
 
 }
