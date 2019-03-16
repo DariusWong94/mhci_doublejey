986b042a867b5bd802a6e8d8385751b80ca46022
Darius Wong <dariuswong94@gmail.com>
2019-03-16 16:38:37 +0800

    - improved UI

--- app/src/main/java/com/ardiya/simpleweather/RankFragment.java
+++ app/src/main/java/com/ardiya/simpleweather/RankFragment.java
@@ -31,14 +31,19 @@ public class RankFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_rank, container, false);
 
         String[] listviewTitle = new String[]{
-                "ListView Title 1", "ListView Title 2", "ListView Title 3", "ListView Title 4", "ListView Title 10",
-                "ListView Title 5", "ListView Title 6", "ListView Title 7", "ListView Title 8", "ListView Title 9"
+                "Dom Tiang", "Halimin", "Tan Jun He", "Yi Han", "Jovi Yeo","Gerry Kor",
+                "Rapheal Chua", "Darius Wong", "Toh Yu Heng", "Ethan Chin"
         };
 
 
         int[] listviewImage = new int[]{
                 R.drawable.rankusers, R.drawable.rankusers, R.drawable.rankusers, R.drawable.rankusers, R.drawable.rankusers,
                 R.drawable.rankusers, R.drawable.rankusers, R.drawable.rankusers, R.drawable.rankusers, R.drawable.rankusers
+        };
+
+        int[] listviewIcon = new int[]{
+                R.drawable.zzz_trophy_variant_outline, R.drawable.zzz_trophy_variant_outline, R.drawable.zzz_trophy_variant_outline, R.drawable.zzz_star, R.drawable.zzz_star,
+                R.drawable.zzz_star, R.drawable.zzz_star, R.drawable.zzz_star, R.drawable.zzz_star, R.drawable.zzz_star
         };
 
         String[] listviewShortDescription = new String[]{
@@ -55,11 +60,12 @@ public class RankFragment extends Fragment {
             hm.put("listview_title", listviewTitle[i]);
             hm.put("listview_discription", listviewShortDescription[i]);
             hm.put("listview_image", Integer.toString(listviewImage[i]));
+            hm.put("listview_icon", Integer.toString(listviewIcon[i]));
             aList.add(hm);
         }
 
-        String[] from = {"listview_image", "listview_title", "listview_discription"};
-        int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description};
+        String[] from = {"listview_image", "listview_title", "listview_discription", "listview_icon" };
+        int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description, R.id.listview_icon};
 
         SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(), aList, R.layout.listview_activity, from, to);
         ListView androidListView = (ListView) view.findViewById(R.id.listview);
